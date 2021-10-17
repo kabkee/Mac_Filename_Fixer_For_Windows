607d2ba1d71d010100b403d834955f145eb57118
Kabkee <kabkee.moon@gmail.com>
2021-10-17 18:09:09 +0900

    윈도우창 크기 조절 세로 500

--- src/background.js
+++ src/background.js
@@ -18,7 +18,7 @@ async function createWindow() {
     // Create the browser window.
     const win = new BrowserWindow({
         width: 500,
-        height: 300,
+        height: 500,
         webPreferences: {
             // Use pluginOptions.nodeIntegration, leave this alone
             // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
